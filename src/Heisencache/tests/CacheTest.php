1a2ffcc11bc51bee7e479c9ff83f44ae4b27d0bb
Frederic G. MARAND <fgm@osinet.fr>
2014-08-01 14:47:02 +0200

    Issue #4: CacheTest demonstrates Cache::get() is broken.
    - made heisencache_require() more consistent.
    - fixed incorrect escaping in class name in Cache.
    - fixed missing LF at the end of heisencache.install.

---
+++ src/Heisencache/tests/CacheTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @file
+ * Unit tests for the Cache class.
+ *
+ * @author: Frédéric G. MARAND <fgm@osinet.fr>
+ *
+ * @copyright (c) 2014 Ouest Systèmes Informatiques (OSInet).
+ *
+ * @license General Public License version 2 or later
+ */
+
+namespace OSInet\Heisencache\tests;
+
+use OSInet\Heisencache\Cache;
+use OSInet\Heisencache\Config;
+
+class CacheTest extends \PHPUnit_Framework_TestCase {
+
+  /**
+   * Ensure Cache::get() actually returns the data from the original handler.
+   *
+   * https://github.com/FGM/heisencache/issues/4
+   */
+  public function testGet4() {
+    $expected = "some random string";
+    $cid = "testGet4";
+    $bin = 'cache';
+
+    // Create a mock DB cache which will return $expected.
+    $handler = $this->getMockBuilder('\DrupalDatabaseCache')
+      ->setMethods(array('get'))
+      ->setConstructorArgs(array($bin))
+      ->getMock();
+    $handler->expects($this->once())
+      ->method('get')
+      ->will($this->returnValue($expected));
+
+    // Create a fake config
+    if (!isset($GLOBALS['conf'])) {
+      $GLOBALS['conf'] = array();
+    }
+    $config = Config::instance(array());
+    $GLOBALS['conf'] = $config->override();
+
+    // Override default override with our mock cache. Since this is not a public
+    // property and there is no setter, force it using reflection.
+    $cache = new Cache('cache');
+    $rp = new \ReflectionProperty($cache, 'handler');
+    $rp->setAccessible(TRUE);
+    $rp->setValue($cache, $handler);
+
+    $actual = $cache->get($cid);
+    $this->assertNotEquals($actual, FALSE, "Cache returns permanent stored data");
+  }
+}
